48e2d9c3d4b7323c461d1e941158ad74586cc533
Rovia <rovia@Abhisheks-MacBook-Air.local>
2025-12-17 22:40:44 +0530

    Resolve config conflicts and add worklets plugin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   DarkTheme,
   DefaultTheme,
@@ -21,36 +20,18 @@ if (
 ) {
   UIManager.setLayoutAnimationEnabledExperimental(true);
 }
-=======
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
->>>>>>> 4b0fe7b (Initial commit)
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-<<<<<<< HEAD
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
-=======
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
->>>>>>> 4b0fe7b (Initial commit)
     return null;
   }
 
   return (
-<<<<<<< HEAD
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <LastTabProvider>
         <CartProvider>
@@ -79,14 +60,6 @@ export default function RootLayout() {
           </LocationProvider>
         </CartProvider>
       </LastTabProvider>
-=======
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
->>>>>>> 4b0fe7b (Initial commit)
     </ThemeProvider>
   );
 }
